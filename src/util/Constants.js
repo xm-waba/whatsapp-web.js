3ca6c70efdc81fcba9b17973d103ed3ec40eea99
xm-waba <xm-waba@users.noreply.github.com>
2026-09-12 18:32:21 +0000

    Update supported WhatsApp Web version to v2.3000.1047399436

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -7,7 +7,7 @@ exports.DefaultOptions = {
         headless: true,
         defaultViewport: null
     },
-    webVersion: '2.3000.1017054665',
+    webVersion: '2.3000.1047399436',
     webVersionCache: {
         type: 'local',
     },
